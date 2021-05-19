beebab42b519a688e5c59f3667004795f5b01ac6
Ashraf Sao <Ashraf.Sao89@Gmai.com>
2021-05-19 15:04:19 +0300

    hotfix

--- apps.js
+++ apps.js
@@ -1,63 +1,97 @@
-'use srtict'
+'use strict'
+
 const form = document.getElementById('form');
-const table = document.getElementById('table-container');
-const tbody = document.createElement('tbody');
+const tableContainer = document.getElementById('table-container');
+const table = document.createElement('table');
 
-myFlowers.allInsertedFlowers = JSON.parse(localStorage.getItem('flowers')) || [];
 
-function myFlowers(name, image, season) {
-    this.name = name;
+myFlowers.allInsertedFlowers = JSON.parse(localStorage.getItem('Flowers')) || [];
+
+function myFlowers(title, category) {
+    this.title = title;
+    this.category = category;
     this.season = season;
-    this.image = image;
 
     myFlowers.allInsertedFlowers.push(this);
-
+    localStorage.setItem('flowers', JSON.stringify(myFlowers.allInsertedFlowers))
 }
 
-form.addEventListener('submit', function (e) {
-    e.preventdefault();
+let flowersObject;
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const flowersName = form.flowersName;
+    const flowersCategory = form.flowersCategory;
+    new myFlowers(flowersName.value, flowersCategory.value);
+    drawTableBody();
+
+    flowersName.value = ""
 
 
-});
 
-const flowerName = form.flowerName
-const flowerSeason = form.flowerSeason
-const flowerImage = form.flowerImage
+})
 
-new myFlowers(flowerName.value, flowerImage.Value, flowerSeason.value);
+function drawTableHeader() {
+    let thead = document.createElement('thead');
+    let trow = document.createElement('tr');
 
+    let colName = document.createElement('td');
+    let colCategory = document.createElement('td');
+    let colSeason = document.createElement('td');
+    let colRemove = document.createElement('td');
 
-drawTableBody();
-flowerName.value = '';
+    colName.textContent = 'flowers Name';
+    colCategory.textContent = 'flowers Category';
+    colSeason.textContent = 'flowers Season';
+    colRemove.textContent = 'Remove';
+
+    trow.appendChild(colName);
+    trow.appendChild(colCategory);
+    trow.appendChild(colSeason);
+    trow.appendChild(colRemove);
+
+    thead.appendChild(trow);
+
+    table.appendChild(thead);
+
+    tableContainer.appendChild(table);
+}
 
 function drawTableBody() {
+    const tbody = document.createElement('tbody');
     tbody.innerHTML = '';
 
-    for (let i = 0; i < myFlowers.allInsertedFlowers.lengh; i++) {
+    for (let i = 0; i < myFlowers.allInsertedFlowers.length; i++) {
+
         const trow = document.createElement('tr');
         const colName = document.createElement('td');
-        const colImage = document.createElement('td');
+        const colCategory = document.createElement('td');
         const colSeason = document.createElement('td');
         const colRemove = document.createElement('td');
 
-        const removeButton = document.createElement('button')
-        removeButton.setAttribute('id', myFlowers.allInsertedFlowers[i].name);
-        removeButton.setAttribute('class', 'remove-button');
-        removeButton.addEventListener('click', function () {
-            removeFlowerFromDomAndLocalStorage(i)
-        });
+        const removeButton = document.createElement('button');
+
+
+        colName.textContent = myFlowers.allInsertedFlowers[i].title;
+        colCategory.textContent = myFlowers.allInsertedFlowers[i].category;
+        colSeason.textContent = myFlowers.allInsertedFlowers[i].season
+        colRemove.textContent = 'X'
+
+        colRemove.appendChild(removeButton);
+
+        trow.appendChild(colName);
+        trow.appendChild(colCategory);
+        trow.appendChild(colSeason);
+        trow.appendChild(colRemove);
+        tbody.appendChild(trow);
+
     }
 
+
+    table.appendChild(tbody);
+
+    tableContainer.appendChild(table)
+
 }
-
-colName.textcontent = myFlowers.allInsertedFlowers[i].name;
-colSeason.textcontent = myFlowers.allInsertedFlowers[i].season
-colImage.textcontent = myFlowers.allInsertedFlowers[i].image
-
-
-function removeFlowerFromDomAndLocalStorage(index) {
-    myFlowers.allInsertedFlowers.splice(index, 1);
-    localStorage.setItem('flowers', JSON.stringify(myFlowers.allInsertedFlowers));
-
-    drawTableBody()
-}
+drawTableBody();    
+drawTableBody();
